077f1c2f071776debe8f9d77ab8c49bffc6d3f66
Harsh lal <143641863+lalharsh23@users.noreply.github.com>
2024-11-29 12:33:45 +0530

    Update Signup.js

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -11,7 +11,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5001/api/auth/register", {
+      const res = await fetch("https://mern-auth-system.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password, role }),
