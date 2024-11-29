fe331043f19e1826c694f86d152cc2d095859244
Harsh lal <143641863+lalharsh23@users.noreply.github.com>
2024-11-29 12:33:28 +0530

    Update Signin.js

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -10,7 +10,7 @@ const Signin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5001/api/auth/login", {
+      const res = await fetch("https://mern-auth-system.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
